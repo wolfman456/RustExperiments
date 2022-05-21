9985b3432bdfc8d9f889bd041fc485cb756cb418
Paul Shook <pshook456@gmail.com>
2022-05-21 18:03:12 -0500

    first program

---
+++ hello/target/debug/deps/hello-e5cc7a191a1d1056.d
@@ -0,0 +1,5 @@
+/home/wolfman456/project/rust/learning/RustExperiments/hello/target/debug/deps/hello-e5cc7a191a1d1056: src/main.rs
+
+/home/wolfman456/project/rust/learning/RustExperiments/hello/target/debug/deps/hello-e5cc7a191a1d1056.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/wolfman456/project/rust/learning/RustExperiments/hello/target/debug/hello: /home/wolfman456/project/rust/learning/RustExperiments/hello/src/main.rs
