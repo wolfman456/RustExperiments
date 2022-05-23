2cbab28eb05f83a84335c5e656e52a36a3b65d61
Paul Shook <pshook456@gmail.com>
2022-05-23 18:38:25 -0500

    continued

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("Hello, world!");
     println!("my name is {}", "bob");
+    let name = "smith";
+    println!("my name is bob {}", name);
+    let number = 5 + 5;
+    println!("5 + 5 = {}", number);
 }
